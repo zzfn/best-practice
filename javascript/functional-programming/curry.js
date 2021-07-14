6b476273ce06a7df159d4330d250839e4983376d
chenc <chenchen3@fosun.com>
2021-07-14 16:58:58 +0800

    feat: 柯里化

--- javascript/functional-programming/curry.js
+++ javascript/functional-programming/curry.js
@@ -1,22 +1,42 @@
+// const sum = (x, y) => x + y;
+//
+// function sumCurry(a) {
+//     return function (b) {
+//         return a + b
+//     }
+// }
+//
+// console.log(sumCurry(1)(2))
+//
+// function curry(fn, ...arg) {
+//     console.log(fn, arg)
+//     return function (params) {
+//         const p = arg.concat([params])
+//         return fn(...p)
+//     }
+// }
+//
+// var addCurry = curry(function (a, b, c) {
+//     return a + b + c;
+// });
+
+// console.log(addCurry(1)(2)(3));  // 6
+// const curry = fn =>
+//     judge = (...args) =>
+//         args.length === fn.length
+//             ? fn(...args)
+//             : (arg) => judge(...args, arg)
+const curry1 = (fn, ...args) =>
+    args.length >= fn.length
+        ? fn(...args)
+        : (..._args) => curry1(fn, ...args, ..._args);
 
 
-const sum = (x, y) => x + y;
-function sumCurry(a) {
-    return function (b) {
-        return a+b
-    }
-}
-
-console.log(sumCurry(1)(2))
-function curry(fn,...arg) {
-    console.log(fn,arg)
-    return function (params) {
-        const p=arg.concat([params])
-        return fn(...p)
-    }
-}
-var addCurry=curry(function(a, b, c) {
-    return a + b + c;
+var fn = curry1(function(a, b, c) {
+    return [a, b, c];
 });
-
-console.log(addCurry(1)(2)(3));  // 6
+const currAdd=curry1(fn)
+console.log(currAdd(1,2,3))
+console.log(currAdd(1)(2)(3))
+console.log(currAdd(1)(2,3))
+const curry=(fn,...arg)=>arg.length>=fn.length?fn(...arg):(..._arg)=>curry(fn,...arg,..._arg)
