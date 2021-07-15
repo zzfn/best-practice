1748f238782550c5152ab4503d8e74ad61aedca5
chenc <chenchen3@fosun.com>
2021-07-15 19:15:31 +0800

    feat: 缓存

--- javascript/other/Hooks.js
+++ javascript/other/Hooks.js
@@ -3,38 +3,49 @@ let workInProgressHook = null
 const fiber = {
     stateNode: App,
     memoizedStated: null,
-    queue:{
-        pending:null
+    queue: {
+        pending: null
     }
 }
-function dispatchAction(queue,action){
-const update={
-    action,
-    next:null
+
+function dispatchAction(queue, action) {
+    const update = {
+        action,
+        next: null
+    }
+    if(queue===null){
+        update.next=update
+    }else {
+        update.next=queue.pending.next
+        queue.pending.next=update
+    }
+    queue.pending=update
+    schedule()
 }
-}
+
 function useState(initialState) {
     let hook;
-    if(isMount){
-        hook={
-            memoizedStated:initialState,
-            next:null
+    if (isMount) {
+        hook = {
+            memoizedStated: initialState,
+            next: null
         }
-        if(!fiber.memoizedStated){
-            fiber.memoizedStated=hook
-        }else {
-            workInProgressHook.next=hook
+        if (!fiber.memoizedStated) {
+            fiber.memoizedStated = hook
+        } else {
+            workInProgressHook.next = hook
         }
-        workInProgressHook=hook
-    }else {
-        hook=workInProgressHook
-        workInProgressHook.next=workInProgressHook
+        workInProgressHook = hook
+    } else {
+        hook = workInProgressHook
+        workInProgressHook.next = workInProgressHook
     }
     return []
 }
+
 function schedule() {
     workInProgressHook = fiber.memoizedStated
-    const app=fiber.stateNode()
+    const app = fiber.stateNode()
     isMount = false
     return app
 }
@@ -48,4 +59,5 @@ function App() {
         }
     }
 }
-window.app=schedule()
+
+window.app = schedule()
