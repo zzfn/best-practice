e24093a85a31f1c4267654a3b7ac3086e84db0d8
chenc <chenchen3@fosun.com>
2021-08-03 14:49:54 +0800

    feat: list2tree

--- javascript/other/list2tree.js
+++ javascript/other/list2tree.js
@@ -1,9 +1,22 @@
+const genItems = (count = 1000, group = 4) => {
+    const arr= []
+    let i = 0
+    while(count >= ++i) {
+        arr.push({
+            id: i,
+            pid: (i - 1) / group >> 0,
+            ts: Date.now(),
+            hash: Math.random()
+        })
+    }
+    return arr.sort(() => Math.random() - 0.5)
+}
 
+const arr =genItems(20)
 
-const arr = Array.from({length: 200}).map((node, idx) => ({id: idx, name: `tag-${idx}`, pid: parseInt(Math.random()*(500+1),10)}))
 function list2tree(list) {
     const option = {id: 'id', pid: 'pid', children: 'children'};
-    const a=list.reduce((prev, curr) => {
+    return list.reduce((prev, curr) => {
         const obj = list.find((item) => item[option.id] === curr[option.pid]);
         if (obj) {
             !Object.prototype.hasOwnProperty.call(obj, option.children) && (obj[option.children] = []);
@@ -11,8 +24,12 @@ function list2tree(list) {
         } else {
             prev.push(curr);
         }
+        console.log(curr,prev)
         return prev;
     }, []);
-    console.log(a)
 }
-list2tree(arr)
+
+console.time('list2tree')
+console.log(list2tree(arr))
+console.timeEnd('list2tree')
+
