a51669811aa08da0e529d53c8e3d208041f5ee14
chenc <chenchen3@fosun.com>
2021-08-10 10:06:06 +0800

    feat: deepclone

--- javascript/other/test.js
+++ javascript/other/test.js
@@ -1,8 +1,23 @@
 function Test() {
-    this.c=3
+    this.c = 3
 }
-Test.a=1
-Test.prototype.b=2
 
-console.log(new Test().b)
-console.log(new Test().c)
+Test.a = 1
+Test.prototype.b = 2
+
+class Person {
+    static a = 1
+
+    constructor() {
+        this.c = 3
+    }
+
+    b() {
+        return 2
+    }
+
+    d = 2
+}
+Person.e=3
+const a=new Person()
+console.log(a)
