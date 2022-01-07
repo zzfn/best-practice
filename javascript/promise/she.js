498c929f36e3d9e08dd650ea52201bb5e7a24ba6
cc <44751363+zzfn@users.noreply.github.com>
2022-01-08 01:42:38 +0800

    feat: 并发

---
+++ javascript/promise/she.js
@@ -0,0 +1,81 @@
+const list = [
+    () => request(1, 800),
+    () => request(2, 500),
+    () => request(3, 200),
+    () => request(4, 800),]
+
+/**
+ * 串行
+ * @param urls
+ * @returns {Promise<unknown>}
+ */
+function requestUrls(urls = []) {
+    let result = []
+    return new Promise((resolve, reject) => {
+        function next(paths) {
+            if (paths.length === 0) {
+                resolve(result)
+            } else {
+                const path = paths.shift()
+                path().then(r => {
+                    result.push(r)
+                    next(paths.slice())
+                })
+            }
+        }
+
+        next(urls)
+    })
+}
+
+/**
+ * 并发控制
+ * @returns {Promise<unknown>}
+ * @param urls
+ * @param limit
+ */
+function requestUrlsReq(urls = [], limit = 2) {
+    let result = []
+    let index = 0
+    let count = limit
+    return new Promise((resolve, reject) => {
+        function next() {
+            if (urls.length === 0 && count === limit) {
+                resolve(result)
+            } else if (urls.length) {
+                while (count) {
+                    const idx = index++
+                    const path = urls.shift()
+                    count--
+                    path().then(r => {
+                        count++
+                        result[idx] = r
+                        next()
+                    })
+                }
+            }
+        }
+
+        next()
+    })
+}
+
+function request(url, wait) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(url)
+        }, wait)
+    })
+}
+
+console.time('start')
+requestUrls(list).then(r => {
+    console.log(r)
+    console.timeEnd('start')
+})
+// function test(urls) {
+//     return urls.reduce((prevPromise, url) => prevPromise.then((r) => ), Promise.resolve())
+// }
+// test(list).then(r => {
+//     console.log(r)
+// })
