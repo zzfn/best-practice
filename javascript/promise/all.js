0027051d91b930d008f7da43b68633769e68e54f
chenc <44751363+zzfn@users.noreply.github.com>
2022-01-07 11:30:04 +0800

    feat: 修改promise

--- javascript/promise/all.js
+++ javascript/promise/all.js
@@ -8,7 +8,7 @@ Promise.myAll = function (arr) {
     return new Promise((resolve, reject) => {
         let result=[]
         let count=0
-        for(let n in arr){
+        for(let n in Object.keys(arr)){
             Promise.resolve(arr[n]).then(r=>{
                 result[n]=r
                 ++count
@@ -26,3 +26,34 @@ Promise.myAll(list).then(res => {
 }).catch(e=>{
     console.log(e)
 })
+function requestUrls(urls = []) {
+    let currentUrls = urls.slice(),result = [], pendingPromise = Promise.resolve();
+
+    return new Promise((resolve) => {
+        function next(){
+            pendingPromise.then((data) => {
+                if(data) result.push(data);
+                const url = currentUrls.shift();
+                if(!url) {
+                    resolve(result)
+                    return;
+                };
+
+                pendingPromise = request(url, Math.random() * 2000);
+                next()
+
+            })
+        }
+
+        next();
+
+    })
+
+}
+
+function request(url, wait) {
+    return new Promise(resolve => setTimeout(() => {
+        console.log(url, wait);
+        resolve(url);
+    }, wait));
+}
