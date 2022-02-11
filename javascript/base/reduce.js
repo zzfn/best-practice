d55ebbe6748fedf8be62d70a67ef6885c2a480bf
zzfn <44751363+zzfn@users.noreply.github.com>
2022-02-11 16:52:08 +0800

    feat: reduce

---
+++ javascript/base/reduce.js
@@ -0,0 +1,13 @@
+Array.prototype.myReduce = function (fn, initialValue) {
+    let acc = initialValue;
+    for (let i = 0; i < this.length; i++) {
+        if (acc === undefined) {
+            acc = this[i];
+        } else {
+            acc = fn(acc, this[i], i, this);
+        }
+    }
+    return acc;
+}
+const a = [1, 2, 3, 4, 5];
+console.log(a.myReduce((acc, curr) => acc + curr, 0));
