dccc597562ae8be7231b8650bee42e623a386a64
zzfn <44751363+zzfn@users.noreply.github.com>
2022-02-20 13:41:29 +0800

    feat: fix

--- javascript/base/reduce.js
+++ javascript/base/reduce.js
@@ -1,7 +1,7 @@
 Array.prototype.myReduce = function (fn, initialValue) {
     let acc = initialValue;
     for (let i = 0; i < this.length; i++) {
-        if (acc === undefined) {
+        if (acc === undefined && i === 0) {
             acc = this[i];
         } else {
             acc = fn(acc, this[i], i, this);
--- javascript/functional-programming/compose.js
+++ javascript/functional-programming/compose.js
@@ -12,3 +12,8 @@ const toUpper = (s) => {
     console.log(2);return s.toUpperCase()};
 const f = compose(toUpper, first, reverse);
 console.log(f(["one", "two", "three"])); // 'THREE'
+
+
+
+const compose1 = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
