a86c9d858ef4aac91a995a60095e6659084bed61
zzfn <44751363+zzfn@users.noreply.github.com>
2022-03-05 19:16:20 +0800

    feat: fix

--- javascript/base/asyncToGenerator.js
+++ javascript/base/asyncToGenerator.js
@@ -1,7 +1,7 @@
 function _asyncToGenerator(fn) {
-    return function() {
+    return function () {
         var gen = fn.apply(this, arguments);
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             function step(key, arg) {
                 try {
                     var info = gen[key](arg);
@@ -14,16 +14,58 @@ function _asyncToGenerator(fn) {
                     resolve(value);
                 } else {
                     return Promise.resolve(value).then(
-                        function(value) {
+                        function (value) {
                             step("next", value);
                         },
-                        function(err) {
+                        function (err) {
                             step("throw", err);
                         }
                     );
                 }
             }
+
             return step("next");
         });
     };
+}
+
+let num = 1;
+
+function b() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(num++), 1000);
+    });
+}
+
+function* a() {
+    let value1 = yield b();
+    let value2 = yield b();
+    let value3 = yield b();
+    console.log('---',value1, value2, value3);
+    return value3
+}
+//
+// _asyncToGenerator(a)().then(res => {
+//     console.log(res);
+// });
+Co(a).then(res => {
+    console.log(res);
+});
+function Co(fn) {
+    return new Promise(resolve =>{
+        var gen = fn();
+        function next(err, data) {
+            var result = gen.next(data);
+            if (result.done) {
+                resolve(result.value);
+            } else {
+                result.value.then(function (data) {
+                    next(null, data);
+                }, function (err) {
+                    next(err);
+                });
+            }
+        }
+        next();
+    })
 }
